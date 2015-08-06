91d7d533a79b495b40830dab2070a4f1b9378acc
techdomain0502 <techdomain0502@gmail.com>
2015-08-06 07:50:58 +0530

    play pause feature change
    play pause feature change

--- app/src/main/java/com/test/board/MainActivity.java
+++ app/src/main/java/com/test/board/MainActivity.java
@@ -25,22 +25,118 @@ import java.util.Collections;
 public class MainActivity extends ActionBarActivity {
 
     private Board board;
-
+private TextView hr,min,sec,msec;
+    private long secs,mins,hrs;
+    private String seconds,milliseconds,hours,minutes;
+    private Handler mHandler = new Handler();
+    private long startTime;
+    private long elapsedTime;
+    private final int REFRESH_RATE = 100;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.board);
-        board = (com.test.board.Board)findViewById(R.id.board);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Bundle  i = getIntent().getExtras();
+		  Bundle  i = getIntent().getExtras();
         String grid = i.get("grid").toString();
         if(grid != null)
         Toast.makeText(this,grid,Toast.LENGTH_SHORT).show();
+        board = (com.test.board.Board)findViewById(R.id.board);
+		 hr = (TextView)findViewById(R.id.hour);
+        min = (TextView)findViewById(R.id.min);
+        sec = (TextView)findViewById(R.id.sec);
+        msec = (TextView)findViewById(R.id.msec);
         board.initBoard(3);
     }
+
+    private void updateTimer (float time){
+        secs = (long)(time/1000);
+        mins = (long)((time/1000)/60);
+        hrs = (long)(((time/1000)/60)/60);
+
+		/* Convert the seconds to String
+		 * and format to ensure it has
+		 * a leading zero when required
+		 */
+        secs = secs % 60;
+        seconds=String.valueOf(secs);
+        if(secs == 0){
+            seconds = "00";
+        }
+        if(secs <10 && secs > 0){
+            seconds = "0"+seconds;
+        }
+
+		/* Convert the minutes to String and format the String */
+
+        mins = mins % 60;
+        minutes=String.valueOf(mins);
+        if(mins == 0){
+            minutes = "00";
+        }
+        if(mins <10 && mins > 0){
+            minutes = "0"+minutes;
+        }
+
+    	/* Convert the hours to String and format the String */
+
+        hours=String.valueOf(hrs);
+        if(hrs == 0){
+            hours = "00";
+        }
+        if(hrs <10 && hrs > 0){
+            hours = "0"+hours;
+        }
+
+    	/* Although we are not using milliseconds on the timer in this example
+    	 * I included the code in the event that you wanted to include it on your own
+    	 */
+        milliseconds = String.valueOf((long)time);
+        if(milliseconds.length()==2){
+            milliseconds = "0"+milliseconds;
+        }
+        if(milliseconds.length()<=1){
+            milliseconds = "00";
+        }else
+        milliseconds = milliseconds.substring(milliseconds.length()-3, milliseconds.length()-2);
+
+		/* Setting the timer text to the elapsed time */
+        (hr).setText(hours);
+        (min).setText(minutes);
+        (sec).setText(seconds);
+        (msec).setText(milliseconds);
+    }
+
+
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch(id){
+            case R.id.settings:
+                break;
+            case R.id.play:
+                startTime = System.currentTimeMillis();
+                mHandler.removeCallbacks(startTimer);
+                mHandler.postDelayed(startTimer, 0);
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    private Runnable startTimer = new Runnable()
+    { public void run()
+        { elapsedTime = System.currentTimeMillis() - startTime;
+            updateTimer(elapsedTime);
+            mHandler.postDelayed(this,REFRESH_RATE);
+        } };
 
 }
