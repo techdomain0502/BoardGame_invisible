95c90eebe2e76fa7eb70e98e439d64f7b098f6e1
techdomain0502 <techdomain0502@gmail.com>
2015-08-17 19:02:47 +0530

    Adding splash screen
    Adding splash screen

---
+++ app/src/main/java/com/board/game/sasha/SplashScreen.java
@@ -0,0 +1,15 @@
+package com.board.game.sasha;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by sachin on 8/16/2015.
+ */
+public class SplashScreen extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+    }
+}
