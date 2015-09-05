a517ff5a1e2084c710bcd90cff9d8c317f64a6f4
techdomain0502 <techdomain0502@gmail.com>
2015-09-05 20:37:00 +0530

    versioning
    versioning

--- app/src/main/java/com/board/game/sasha/gui/SplashScreen.java
+++ app/src/main/java/com/board/game/sasha/gui/SplashScreen.java
@@ -1,6 +1,7 @@
 package com.board.game.sasha.gui;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Typeface;
@@ -8,38 +9,35 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.board.game.sasha.R;
+import com.board.game.sasha.commonutils.GlobalConstants;
 
 /**
  * Created by sachin on 8/16/2015.
  */
 public class SplashScreen extends Activity {
-    private SharedPreferences.Editor editor;
     private ImageView start;
     private SharedPreferences gamePrefs;
-    private TextView tvTitle;
+    private TextView tvTitle,appVersion;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-      /*  gamePrefs = getSharedPreferences(GlobalConstants.pref_file,Context.MODE_PRIVATE);
-        if(gamePrefs.getBoolean("splash_done", false)){
-            startActivity(new Intent(getApplicationContext(),FrontPage.class));
-            finish();
-        }
-*/
         setContentView(R.layout.splash);
+        gamePrefs = getSharedPreferences(GlobalConstants.pref_file, Context.MODE_PRIVATE);
         tvTitle = (TextView)findViewById(R.id.title);
+        appVersion = (TextView)findViewById(R.id.version);
         Typeface font = Typeface.createFromAsset(getApplicationContext().getAssets(), "fonts/letteromatic.ttf");
         tvTitle.setTypeface(font);
-       //editor = getSharedPreferences(GlobalConstants.pref_file, Context.MODE_PRIVATE).edit();
+
+        appVersion.setText(String.format(getResources().getString(R.string.version),gamePrefs.getString("version","1.0")));
         start = (ImageView)findViewById(R.id.start);
         start.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-         /*       editor.putBoolean("splash_done", true);
-                editor.commit();
-         */       startActivity(new Intent(getApplicationContext(), FrontPage.class));
+             public void onClick(View v) {
+              startActivity(new Intent(getApplicationContext(), FrontPage.class));
                 finish();
             }
         });
